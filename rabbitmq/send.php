447f11f665451fa2d53d4d833ff0c0e761e7b0dc
Umi Zahroh <umizahroh29@gmail.com>
2020-09-29 21:34:56 +0700

    send.php

--- rabbitmq/send.php
+++ rabbitmq/send.php
@@ -1,17 +1,21 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+$message = $_POST['message'];
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
 
-$msg = new AMQPMessage('Hello World!');
+$msg = new AMQPMessage($message);
 $channel->basic_publish($msg, '', 'hello');
 
-echo " [x] Sent 'Hello World!'\n";
+$_SESSION['response'] = " [x] Sent '" . $message . "'\n";
 
 $channel->close();
 $connection->close();
+
+include 'send-gui.php';
